9f3a4c479ddb8f1cc0d9766dbf5c5f29db408354
Ygor Gabriel <81044944+Ygor-Gabriel@users.noreply.github.com>
2023-05-19 15:12:35 -0300

    Add files via upload

---
+++ HoraDoSistema.java
@@ -0,0 +1,11 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema{
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("A hora do sistema é: ");
+        System.out.println(date.toString());
+    }
+}
