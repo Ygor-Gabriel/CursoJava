a4528f00142334d2fe305a8e3a728d32a03c9a20
Ygor Gabriel <81044944+Ygor-Gabriel@users.noreply.github.com>
2023-05-20 19:51:19 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author marques
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.print("A média é: " + m);*/
+        
+        int x = 4;
+        x += 2;  // x = x +  2
+        System.out.println(x);
+    }
+    
+}
