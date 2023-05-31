2bee6729f131cf246bb88ce45f6f8163be3ecf3b
Ygor Gabriel <81044944+Ygor-Gabriel@users.noreply.github.com>
2023-05-31 20:34:32 -0300

    Add files via upload

---
+++ OperadorTernario/src/main/java/com/mycompany/operadorternario/OperadorTernario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadorternario;
+
+/**
+ *
+ * @author marques
+ */
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        /* teste com maior que
+        int n1, n2,r;
+        n1 = 4;
+        n2 = 8;
+        r = (n1>n2)?0:1;
+        System.out.println(r);*/
+        
+    }
+}
