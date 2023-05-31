48f39164de23bc55f0b0c953c2e2b9b6ed0e2a4c
Ygor Gabriel <81044944+Ygor-Gabriel@users.noreply.github.com>
2023-05-31 20:34:50 -0300

    Add files via upload

---
+++ OperadoresLogicos/src/main/java/com/mycompany/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadoreslogicos;
+
+/**
+ *
+ * @author marques
+ */
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        int x, y, z;
+        x = 4;
+        y = 7;
+        z = 12;
+        boolean r;
+        // verificando se as duas estão felizes r = (x<y && y==z)?true:false;
+        // verificando se uma ou outra está feliz r = (x<y || y==z)?true:false;
+        //verificando se uma ou exclusivamente a outra está feliz r = (x<y  y==z)?true:false;
+        
+        System.out.println(r);
+    }
+}
